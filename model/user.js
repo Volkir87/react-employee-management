7344e2424a8e624417bca2a1e6e02a9e5ea05f35
Kirill V <kirilliuswest@gmail.com>
2020-04-29 18:50:32 -0400

    added basic model queries to obtain user from DB

---
+++ model/user.js
@@ -0,0 +1,38 @@
+const connection = require('../config/db-connection');
+
+class User {
+    constructor(){
+        this.connection = connection;
+    };
+
+    async getById(id){
+        let query = `select user_id, first_name, last_name, status_id, created_date
+        from user
+        where id = ?;`;
+        try {
+			let result = await this.connection.query(query, [id]);
+			return result;
+		}
+		catch(error){
+			throw error;
+		}
+    };
+
+    async create(){
+
+    };
+
+    async getAllDetailsById(id){
+        let query = `select *
+        from user
+        where id = ?;`;
+        try {
+			let result = await this.connection.query(query, [id]);
+			return result;
+		}
+		catch(error){
+			throw error;
+		}
+    };
+
+}
