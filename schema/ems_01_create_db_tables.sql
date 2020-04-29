e5bea3eda27c3ba5edda9a04f579eeaa917a371f
Kirill V <kirilliuswest@gmail.com>
2020-04-29 18:21:18 -0400

    added sql schema files

---
+++ schema/ems_01_create_db_tables.sql
@@ -0,0 +1,84 @@
+drop database if exists rems_db;
+
+create database rems_db;
+
+use rems_db;
+
+CREATE TABLE `user` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `user_id` varchar(20),
+  `first_name` varchar(255),
+  `last_name` varchar(255),
+  `password` varchar(255),
+  `status_id` int,
+  `created_date` timestamp DEFAULT (now()),
+  `created_by` varchar(20)
+);
+
+CREATE TABLE `user_status` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `status` varchar(255),
+  `description` varchar(2047)
+);
+
+CREATE TABLE `user_role` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `user_id` int,
+  `role_id` int,
+  `assigned_date` timestamp DEFAULT (now()),
+  `assigned_by` varchar(20)
+);
+
+CREATE TABLE `role` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(255),
+  `description` varchar(2047),
+  `created_date` timestamp DEFAULT (now()),
+  `created_by` varchar(20)
+);
+
+CREATE TABLE `employee` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `first_name` varchar(255),
+  `last_name` varchar(255),
+  `position_id` int,
+  `manager_id` int,
+  `created_date` timestamp DEFAULT (now()),
+  `created_by` varchar(20)
+);
+
+CREATE TABLE `position` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `title` varchar(255),
+  `department_id` int,
+  `created_date` timestamp DEFAULT (now()),
+  `created_by` varchar(20)
+);
+
+CREATE TABLE `department` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(255)
+);
+
+CREATE TABLE `empSalary` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `employee_id` int,
+  `salary` int,
+  `updated_date` timestamp DEFAULT (now()),
+  `updated_by` varchar(20)
+);
+
+ALTER TABLE `employee` ADD FOREIGN KEY (`manager_id`) REFERENCES `employee` (`id`);
+
+ALTER TABLE `employee` ADD FOREIGN KEY (`position_id`) REFERENCES `position` (`id`);
+
+ALTER TABLE `position` ADD FOREIGN KEY (`department_id`) REFERENCES `department` (`id`);
+
+ALTER TABLE `empSalary` ADD FOREIGN KEY (`employee_id`) REFERENCES `employee` (`id`);
+
+ALTER TABLE `user_role` ADD FOREIGN KEY (`role_id`) REFERENCES `role` (`id`);
+
+ALTER TABLE `user_role` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+
+ALTER TABLE `user` ADD FOREIGN KEY (`status_id`) REFERENCES `user_status` (`id`);
+
---
+++ schema/ems_02_insert_employees.sql
@@ -0,0 +1,38 @@
+use rems_db;
+
+insert into `user_status` (status, description) values
+('Active', 'Active user'),
+('Disabled', 'Disabled user');
+
+insert into `user` (`user_id`, `first_name`, `last_name`, `password`, 
+`status_id`, `created_by`) values 
+('admin', 'Admin', '', NULL, 1, 'system');
+
+insert into `role` (`name`, `description`, `created_by`) values 
+('Admin', 'Provides administrative access', 'system');
+
+insert into `user_role` (`user_id`, `role_id`, `assigned_by`) values 
+(1, 1, 'system');
+
+insert into department (name) values 
+('Management'),
+('Operations'),
+('Sales'),
+('Information Technologies');
+
+insert into position (title, department_id, created_by) values 
+('Director', 1, 'system'),
+('Operations Manager', 2, 'system'),
+('Operator', 2, 'system'),
+('Sales Manager', 3, 'system'),
+('Salesperson', 3, 'system'),
+('Developer', 4, 'system'),
+('Business Analyst', 4, 'system');
+
+insert into employee (first_name, last_name, position_id, manager_id, created_by) values 
+('John', 'Johnson', 1, null, 'system'),
+('Fred', 'Fredson', 2, 1, 'system'),
+('Richard', 'Richardson', 4, 1, 'system'),
+('Dominico', 'Dolce', 3, 2, 'system'),
+('Stefano', 'Gabbana', 3, 2, 'system'),
+('Calvin', 'Klein', 5, 3, 'system');
