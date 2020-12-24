6d86501a374b291c074b9eaefad0c24769113ae8
Kirill V <kirilliuswest@gmail.com>
2020-12-24 15:59:27 -0500

    changed ToolBar to Toolbar

--- components/ApplicationBar.js
+++ components/ApplicationBar.js
@@ -2,7 +2,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Typography from '@material-ui/core/Typography';
 import PeopleAltIcon from '@material-ui/icons/PeopleAlt';
-import Toolbar from '@material-ui/core/ToolBar';
+import Toolbar from '@material-ui/core/Toolbar';
 import axios from 'axios';
 
 const useStyles = makeStyles((theme) => ({
